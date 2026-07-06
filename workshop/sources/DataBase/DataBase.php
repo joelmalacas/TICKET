5cb07deb86419c898ee0778096215542d5fefa5f
joelmalacas <joelmalacas@gmail.com>
2026-07-06 19:52:49 +0100

    CREATE Class & File PHP back-office

---
+++ workshop/sources/DataBase/DataBase.php
@@ -0,0 +1,70 @@
+<?php
+class DataBase {
+
+    public String $host;
+    public int $port;
+    public String $user;
+    public String $pass;
+    public String $db;
+    public ?mysqli $conn = null;
+
+    function __construct($host, $port, $user, $pass, $db) {
+        $this->host = $host;
+        $this->port = $port;
+        $this->user = $user;
+        $this->pass = $pass;
+        $this->db = $db;
+    }
+
+    function connDB() : bool{
+        //Ativa exceções no mysqli
+        mysqli_report(MYSQLI_REPORT_STRICT);
+
+        try {
+            $this->conn = new mysqli($this->host, $this->user, $this->pass, $this->db);
+        } catch (Exception $e) {
+            echo json_encode('Connect DB Failed: ' . $e->getMessage());
+            return false;
+        }
+        return true;
+    }
+
+    function TestDB(): bool {
+        $this->connDB();
+        return $this->conn !=null && $this->conn->ping();
+    }
+
+    function statementDB(String $statement, array $params = []): mixed {
+        $this->connDB();
+        $this->conn->query("USE {$this->db}");
+
+        $stmt = $this->conn->prepare($statement);
+
+        if ($stmt === false)
+            return false;
+
+        if (!empty($params)) {
+            $type = str_repeat('s', count($params));
+            $stmt->bind_param($type, ...$params);
+        }
+
+        $executeSuccess = $stmt->execute();
+
+        if (!$executeSuccess) {
+            $stmt->close();
+            return false;
+        }
+
+        if (str_starts_with(strtoupper($statement), 'SELECT')) {
+            $mysqli_result = $stmt->get_result();
+            $result = $mysqli_result->fetch_all(MYSQLI_ASSOC);
+
+            $mysqli_result->free();
+        } else {
+            $result = true;
+        }
+
+        $stmt->close();
+        return $result;
+    }
+}
---
+++ workshop/sources/Respostas/RespostasAPI.php
@@ -0,0 +1,4 @@
+<?php
+
+include '../DataBase/DataBase.php';
+include '../ENV_CORS/ENVCORS.php';
