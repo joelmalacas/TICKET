ccd47bfa691d6dbd8dc54eaff7b58353e3d02648
joelmalacas <joelmalacas@gmail.com>
2026-07-13 12:39:02 +0100

    Otimizar router.php

--- workshop/sources/Router/router.php
+++ workshop/sources/Router/router.php
@@ -2,24 +2,18 @@
 // router.php
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
-if (str_contains($uri, '/admin')) {
-    require __DIR__ . '/../Users/Admin.php';
-    return true;
-}
+$routes = [
+    '/admin' => __DIR__ . '/../Users/Admin.php',
+    '/users' => __DIR__ . '/../Users/Users.php',
+    '/tickets' => __DIR__ . '/../Tickets/TicketsAPI.php',
+    '/Respostas' => __DIR__ . '/../Respostas/RespostasAPI.php',
+];
 
-if (str_contains($uri, '/users')) {
-    require __DIR__ . '/../Users/Users.php';
-    return true;
-}
-
-if (str_contains($uri, '/tickets')) {
-    require __DIR__ . '/../Tickets/TicketsAPI.php';
-    return true;
-}
-
-if (str_contains($uri, '/Respostas')) {
-    require __DIR__ . '/../Respostas/RespostasAPI.php';
-    return true;
+foreach ($routes as $path => $file) {
+    if (str_starts_with($uri, $path)) {
+        require $file;
+        return true;
+    }
 }
 
 http_response_code(404);
