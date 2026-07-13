43bf518d752095a04f4f565e56022c4f1765a439
joelmalacas <joelmalacas@gmail.com>
2026-07-13 13:00:48 +0100

    Update Front-end

--- workshop/sources/Website/src/api/respostas.js
+++
@@ -1,9 +0,0 @@
-import { api } from './client'
-
-export const respostasApi = {
-  list: (idTicket, token) =>
-    api.getWithBody('/Respostas/RESPOSTAS', { ticket_id: idTicket }, token),
-
-  create: (idTicket, mensagem, token) =>
-    api.post('/Respostas/CREATERESPOSTA', { ticket_id: idTicket, mensagem }, token),
-}
--- workshop/sources/Website/src/components/Spinner.jsx
+++
@@ -1,8 +0,0 @@
-export default function Spinner({ label = 'A carregar…' }) {
-  return (
-    <div className="flex items-center gap-3 text-desk-paper2/70 py-10 justify-center">
-      <span className="w-4 h-4 border-2 border-desk-paper2/30 border-t-amber-stamp rounded-full animate-spin" />
-      <span className="font-mono text-sm">{label}</span>
-    </div>
-  )
-}
